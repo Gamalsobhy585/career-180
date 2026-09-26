166b9fbd49a75236ebe3252ef319217dcaeca3ca
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2026-09-26 18:20:19 +0300

    updates in instructoralancetable resource

--- app/Filament/Resources/InstructorBalances/Tables/InstructorBalancesTable.php
+++ app/Filament/Resources/InstructorBalances/Tables/InstructorBalancesTable.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Resources\InstructorBalances\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
